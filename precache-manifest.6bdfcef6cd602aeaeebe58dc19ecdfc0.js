b1faeb4907f5abe90c926db090879ed26ef009ae
Hui <hfangcareer@gmail.com>
2020-05-07 23:20:40 -0400

    Updates

---
+++ precache-manifest.6bdfcef6cd602aeaeebe58dc19ecdfc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44cd117171beddde067e9c96b17ff150",
+    "url": "/Profilmable2.0/index.html"
+  },
+  {
+    "revision": "445e538f0edc1f3792b7",
+    "url": "/Profilmable2.0/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "61ab68a766bd78111dd2",
+    "url": "/Profilmable2.0/static/js/2.3e2f1abc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Profilmable2.0/static/js/2.3e2f1abc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "445e538f0edc1f3792b7",
+    "url": "/Profilmable2.0/static/js/main.9f51d5db.chunk.js"
+  },
+  {
+    "revision": "dadd4457e2e38e14b59c",
+    "url": "/Profilmable2.0/static/js/runtime-main.0fac2c1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Profilmable2.0/static/media/logo.5d5d9eef.svg"
+  }
+]);
