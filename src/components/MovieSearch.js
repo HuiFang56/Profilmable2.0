67a2558c5a8156385d6dc1b7625e5f81f2e3cce4
Hui <hfangcareer@gmail.com>
2020-05-12 01:03:08 -0400

    first data fetch

--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 
 class MovieSearch extends React.Component{
 constructor(props){
     super(props);
     this.state={
-        inputValue:'A movie Name',
+        inputValue:'',
+        movieId:['blank space'],
+        movieRawData:[],
+        movieBf:[]
     }
     this.handleChange = this.handleChange.bind(this);
-}
-
-handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("We hit!");
+    this.handleSubmit = this.handleSubmit.bind(this)
 }
 
 handleChange = (e) =>{
@@ -20,6 +20,26 @@ handleChange = (e) =>{
     });
 }
 
+handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("We hit!");
+    console.log(`${this.state.inputValue}`);
+    // fetch data
+    const fetchData = axios.get(`https://api.themoviedb.org/3/search/movie?api_key=4ac5617406e7160eb1ce718f294179fd&query=${this.state.inputValue}`)
+        .then(
+            response => 
+                this.setState({
+                    movieRawData:response.data.results[0],
+                    movieId:response.data.results[0].id,
+                    movieBf:response.data.results[0].overview
+
+                })
+        ).then(
+            console.log(this.state.movieId),
+            console.log(this.state.movieRawData)
+        )
+        
+}
 
 render(){
 
@@ -33,6 +53,8 @@ render(){
                 </label>
             <input type="submit" value="Submit" />
             </form>
+            <h4>{this.state.movieBf}</h4>
+          
         </div>
     );
     }
